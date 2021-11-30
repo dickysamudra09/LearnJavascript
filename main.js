06811ff7a027b59b7b9170d4ea8e1ff06f7c26d1
Dicky Samudra Alamsyah <dickyalamsyah0901@gmail.com>
2021-11-30 14:13:13 +0700

    Init main.js & rangkuman
    Menambahkan file main dengan format javascript (js) & dasar rangkuman javascript

---
+++ main.js
@@ -0,0 +1,17 @@
+// Untuk menampilkan pesan ke consol
+console.log("Hello Dicky")
+
+// Baris kode ini merupakan expression karena interpreter akan membaca kode ini dan menghasilkan nilai 5.
+5;
+
+// Baris kode ini juga merupakan expression. Interpreter mengevaluasi kode dan juga akan menghasilkan nilai 5.
+2 + 3
+
+
+// declaration statement
+
+var yourName;
+
+let yourAge;
+
+const numberOfDays; //
